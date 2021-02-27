0a8a0135263294de8a20c6f06c75ff850bac8b21
JesusfGarcia <jesusfgarciavaldez@gmail.com>
2021-02-27 10:52:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+const MyComponent = (props) => {
+  const [value, setValue] = React.useState("");
+
+  React.useEffect(() => {
+    console.log("yo me ejecuto al inicio");
+  }, [value]);
+
+  return (
+    <div>
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="ingrese su nombre"
+      />
+    </div>
+  );
+};
+
+function OtherComponent({ name }) {
+  return <div>hola, bienvenido {name}</div>;
+}
+
+class ComponentClass extends React.Component {
+  constructor(props) {
+    console.log("hola desde el constructor");
+    super();
+    this.state = {
+      name: "",
+      phone: "",
+      date: new Date(),
+    };
+  }
+
+  changeName = (e) => {
+    const { value, name } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  onClick = () => {
+    alert(`Bienvenido ${this.state.name}`);
+  };
+
+  componentWillMount() {
+    console.log("yo voy antés");
+  }
+
+  componentDidMount() {
+    this.timeID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hola mundo</h1>
+        <h2>La hora es: {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
+
+const Carta = (props) => {
+  return <div className="carta">{props.children}</div>;
+};
+
+const array = ["Jesús", "Gabriel", "Noemi", "Luis", 'Sofía'];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      {array.map((name) => {
+        return <Carta>Bienvenido {name}</Carta>;
+      })}
     </div>
   );
 }
